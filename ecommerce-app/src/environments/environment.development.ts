af09e078474c221a23ed184cf3aef14843e3a8ec
mora9916 <sthinkholelapeno@gmail.com>
2025-11-27 21:31:28 -0600

    Ajustes para el entorno de productino

--- ecommerce-app/src/environments/environment.development.ts
+++ ecommerce-app/src/environments/environment.development.ts
@@ -1,3 +1,4 @@
 export const environment = {
+    production: false,
     BACK_URL: 'https://localhost:3000/api'
 };
--- ecommerce-app/src/environments/environment.ts
+++ ecommerce-app/src/environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
+    production: true,
     BACK_URL: 'https://gb-v2-5l5y.onrender.com'
 };
